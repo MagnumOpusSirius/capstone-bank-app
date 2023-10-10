71a5d2f229157223fa1bca44600f6e16b72f996a
a1kolawole <a1kolawole@student.bridgew.edu>
2023-10-10 12:45:59 -0400

    Added Home Component

---
+++ bankapp-frontend/Components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Login from "./Login";
+import Navbar from "./Navbar";
+
+function Home() {
+    return(
+        <Navbar/>,
+        <Login/>
+    )
+    
+}
+
+export default Home
