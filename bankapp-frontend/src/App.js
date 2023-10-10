e0e0c325c8857795fc4abdf6f02e65595de9334b
a1kolawole <a1kolawole@student.bridgew.edu>
2023-10-10 15:42:49 -0400

    installed bootstrap

--- bankapp-frontend/src/App.js
+++ bankapp-frontend/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App">
+        <Navbar/>,
         <Home/>
       </header>
     </div>
