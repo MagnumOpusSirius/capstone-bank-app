93e2b7996aea16bc648565aa9af4ca257639cf5e
a1kolawole <a1kolawole@student.bridgew.edu>
2023-10-10 16:26:54 -0400

    removed "management" from navbar

--- bankapp-frontend/src/Components/Navbar.js
+++ bankapp-frontend/src/Components/Navbar.js
@@ -5,21 +5,7 @@ const Navbar = () => {
   return (
     <>
       <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-        <a class="navbar-brand" href="#">
-          Management Portal
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarsExampleDefault"
-          aria-controls="navbarsExampleDefault"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
+        
         <div class="collapse navbar-collapse" id="navbarsExampleDefault">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item">
