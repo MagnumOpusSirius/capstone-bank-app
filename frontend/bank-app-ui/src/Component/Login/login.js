d399862b62906b5137974fe552872bfc258bf751
Parth Mahale <103008535+MagnumOpusSirius@users.noreply.github.com>
2023-10-14 21:58:30 -0700

    trying to generate jwt during login

--- frontend/bank-app-ui/src/Component/Login/login.js
+++ frontend/bank-app-ui/src/Component/Login/login.js
@@ -1,27 +1,51 @@
 // src/LoginForm.js
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './login.css'; 
-import logo from '../logo.png'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./login.css";
+import logo from "../logo.png";
+import axios from "axios";
 
-const LoginForm = ({ onLogin }) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate(); // create a navigate function
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     // Add your authentication logic here
-    if (username && password) {
-      onLogin(username);
+    // if (username && password) {
+    //   onLogin(username);
+    // }
+    //========CHANGES========
+    try {
+      //send an authentication request to the server
+      const response = await axios.post(
+        "http://localhost:8086/api/customer/authenticate",
+        { username, password }
+      );
+
+      //check if authentication is successful and a JWT token was received
+      if (response.data && response.data.jwtToken) {
+        //store the token in the local storage
+        localStorage.setItem("jwtToken", response.data.jwtToken);
+        //navigate to the dashboard
+        navigate("/dashboard");
+      } else {
+        alert("Login Failed!");
+      }
+    } catch (error) {
+      //handle error
+      console.error("Authentication error:", error);
+      alert("Login Failed!");
     }
   };
 
   return (
     <div className="login-container">
-      <div className = "top-corner">
+      <div className="top-corner">
         <Link to="/staff-login">Staff Corner</Link>
       </div>
-      <div className = "top-logo">
+      <div className="top-logo">
         <img src={logo} width={80} height={80} alt="Bank Logo" />
       </div>
       <div className="bank-logo">
@@ -30,8 +54,8 @@ const LoginForm = ({ onLogin }) => {
       <div className="login-form">
         <h2>Bank Login</h2>
         <form onSubmit={handleSubmit}>
-        <label htmlFor="username">Username:</label>
-        <input
+          <label htmlFor="username">Username:</label>
+          <input
             type="text"
             id="username"
             value={username}
@@ -48,8 +72,10 @@ const LoginForm = ({ onLogin }) => {
           />
           {/* ... */}
           <button type="submit">Login</button>
-          <Link to="/forgot-password" className='forget-password'>Forget Password?</Link>
-      
+          <Link to="/forgot-password" className="forget-password">
+            Forget Password?
+          </Link>
+
           <div className="form-links">
             Not A Customer?
             <Link to="/register">Register Here</Link>
